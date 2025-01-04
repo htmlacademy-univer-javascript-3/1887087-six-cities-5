c1e446417388639cb398ed07ccd12553c25f9314
PavelLovygin <55824376+PavelLovygin@users.noreply.github.com>
2025-01-04 13:25:28 +0500

    feat: подогнал под тз

--- src/components/offers/card/offer-card.tsx
+++ src/components/offers/card/offer-card.tsx
@@ -1,13 +1,9 @@
 import {Premium} from './premium.tsx';
 import {Link} from 'react-router-dom';
-import clsx from 'clsx';
 import {offerCardStyles, OfferCardType} from './offer-card-styles.ts';
 import {Offer} from '../../../types/offer.ts';
-import {useAppDispatch, useAppSelector} from '../../../store/hooks.ts';
-import {getAuthorizationStatus} from '../../../store/user/user.selectors.ts';
-import {AppRoute, AuthorizationStatus} from '../../../consts.ts';
-import {redirectToRoute} from '../../../store/actions.ts';
-import {editFavorites} from '../../../store/api-actions.ts';
+import {Bookmark} from '../../bookmarks/bookmark.tsx';
+import {BookmarkStyle} from '../../bookmarks/bookmark-styles.ts';
 
 type OfferCardProps = {
   Offer: Offer;
@@ -17,22 +13,8 @@ type OfferCardProps = {
 
 export function OfferCard(props: OfferCardProps) {
 
-  const authorizationStatus = useAppSelector(getAuthorizationStatus);
-  const dispatch = useAppDispatch();
-  const handleBookmarkClick = () => {
-    if (authorizationStatus !== AuthorizationStatus.Auth) {
-      dispatch(redirectToRoute(AppRoute.Login));
-    } else {
-      dispatch(
-        editFavorites({
-          offerId: props.Offer.id,
-          isFavoriteNow: props.Offer.isFavorite,
-        })
-      );
-    }
-  };
-
   const styles = offerCardStyles[props.OfferCardType];
+
   return (
     <article className={`${styles.classPrefix}__card place-card`}>
       {props.Offer.isPremium ? <Premium/> : null}
@@ -55,25 +37,16 @@ export function OfferCard(props: OfferCardProps) {
                 /&nbsp;night
             </span>
           </div>
-          <button
-            className={clsx('place-card__bookmark-button', 'button', props.Offer.isFavorite && 'place-card__bookmark-button--active')}
-            type="button"
-            onClick={handleBookmarkClick}
-          >
-            <svg className="place-card__bookmark-icon" width={18} height={19}>
-              <use xlinkHref="#icon-bookmark"/>
-            </svg>
-            <span className="visually-hidden">In bookmarks</span>
-          </button>
+          <Bookmark Offer={props.Offer} BookmarkStyle={BookmarkStyle.OfferCard} />
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: `${props.Offer.rating * 20}%`}}/>
+            <span style={{width: `${Math.round(props.Offer.rating) * 20}%`}}/>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name" >
-          <Link to={`offer/${props.Offer.id}`}>{props.Offer.title}</Link>
+          <Link to={`/offer/${props.Offer.id}`}>{props.Offer.title}</Link>
         </h2>
         <p className="place-card__type">{props.Offer.type}</p>
       </div>
--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -2,18 +2,32 @@ import {FavoritesOfferCardList} from '../../components/favorites/favorite-city-s
 import {NavBar} from '../../components/nav-bar/nav-bar.tsx';
 import {Link} from 'react-router-dom';
 import {AppRoute} from '../../consts.ts';
+import {useAppSelector} from '../../store/hooks.ts';
+import {getFavoriteOffers} from '../../store/user/user.selectors.ts';
 
 export function Favorites() {
+  const favoritesOffers = useAppSelector(getFavoriteOffers);
+
   return (
     <>
       <NavBar/>
       <div className="page">
         <main className="page__main page__main--favorites">
           <div className="page__favorites-container container">
-            <section className="favorites">
-              <h1 className="favorites__title">Saved listing</h1>
-              <FavoritesOfferCardList />
-            </section>
+            {favoritesOffers.length === 0 ?
+              <section className="favorites favorites--empty">
+                <h1 className="visually-hidden">Favorites (empty)</h1>
+                <div className="favorites__status-wrapper">
+                  <b className="favorites__status">Nothing yet saved.</b>
+                  <p className="favorites__status-description">Save properties to narrow down search or plan your future
+                    trips.
+                  </p>
+                </div>
+              </section> :
+              <section className="favorites">
+                <h1 className="favorites__title">Saved listing</h1>
+                <FavoritesOfferCardList/>
+              </section>}
           </div>
         </main>
         <footer className="footer container">
